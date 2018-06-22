3c5e46476329d3c93c5dead60d90812e8cdc11a3
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-06-22 15:12:27 -0700

    sounds complete
    also added ScatMan recordings

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVKit
+import AudioToolbox
+import AVFoundation //  for m4a sound
 
 class ViewController: UIViewController{
     
+//    var player : AVAudioPlayer?
+//    let soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
+    let recordingArray = ["skiBiDeep", "bweeb", "bopBop", "baDopBop", "diBiDeep", "imA","scatMan"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +25,23 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        print(sender.tag)
+
+        //  Xylaphone sounds
+//        if let soundURL = Bundle.main.url(forResource : soundArray[sender.tag - 1], withExtension : "wav"){
+//            var mySound: SystemSoundID = 0
+//            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+//            // Play
+//            AudioServicesPlaySystemSound(mySound)
+//        }
+        //  ScatMan sounds
+        if let soundURL = Bundle.main.url(forResource : recordingArray[sender.tag - 1], withExtension : "m4a"){
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            // Play
+            AudioServicesPlaySystemSound(mySound)
+        }
+
     }
     
   
